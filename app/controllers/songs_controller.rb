9d94c26a0be2f58e5ad1fbd0ba284499c8e8ad9f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-09 19:21:56 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,6 @@ class SongsController < ApplicationController
       @songs = Artist.find(params[:artist_id]).songs
     else
       @songs = Song.all
-      redirect_to :songs_path
     end
   end
 
